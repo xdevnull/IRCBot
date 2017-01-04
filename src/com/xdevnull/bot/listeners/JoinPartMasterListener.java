655dc7eabbc18ddb79d89ed4fbda7db1675ca2c2
/dev/null <arabubaker12@cit.just.edu.jo>
2017-01-05 01:24:05 +0200

    Create JoinPartMasterListener.java

---
+++ src/com/xdevnull/bot/listeners/JoinPartMasterListener.java
@@ -0,0 +1,33 @@
+package com.xdevnull.bot.listeners;
+
+import com.xdevnull.bot.net.EventListener;
+import com.xdevnull.bot.net.events.PrivateMessageEvent;
+import com.xdevnull.bot.script.Permission;
+
+/**
+ * 
+ * @author xdevnull
+ * @date Jan 5, 2017
+ */
+public class JoinPartMasterListener extends EventListener {
+
+	public void onPrivateMessageEvent(PrivateMessageEvent e) {
+		Permission permission = e.getBot().getConfiguration().getPermission();
+		if (permission == null)
+			return;
+		if(permission.has(e.getNickname())) {
+			if(e.getMessage().startsWith("--join")) {
+				String[] split = e.getMessage().split(" ");
+				if(split.length == 0 && split[1].charAt(0) == '#') {
+					e.getBot().sendRaw("JOIN " + split[1]);
+				}
+			}
+			else if(e.getMessage().startsWith("--part")) {
+				String[] split = e.getMessage().split(" ");
+				if(split.length == 0 && split[1].charAt(0) == '#') {
+					e.getBot().sendRaw("PART " + split[1]);
+				}
+			}
+		}
+	}
+}
